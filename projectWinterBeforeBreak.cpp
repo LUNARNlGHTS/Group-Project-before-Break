e3170c0b520d980f5b5c8580088f27f3f4bf36e8
austin-hk <austinhekk@gmail.com>
2025-12-16 10:42:49 -0500

    Update projectWinterBeforeBreak.cpp

--- projectWinterBeforeBreak.cpp
+++ projectWinterBeforeBreak.cpp
@@ -15,6 +15,14 @@ int main() {
     int enemy1Health, enemy1Gold, enemy1Status, enemy1Defense, enemy1Attack;
     int enemy2Health, enemy2Gold, enemy2Status, enemy2Defense, enemy2Attack;
     int enemy3Health, enemy3Gold, enemy3Status, enemy3Defense, enemy3Attack;
+
+    // class declarations for basic, glass cannon, and tank
+
+    // main menu code would be here, including name specifics and class choosing
+
+    // assuming general combat code would be here, cycling 3 enemies before going to the shop
+
+    // shop code would be here, then combat would repeat same with shop every 3 rounds
  
     
 }
