cb69f2410972406ba7976842608cd6965abb6e14
Athon Dulin <90697@wws.k12.in.us>
2025-12-16 10:28:01 -0500

    Update projectWinterBeforeBreak.cpp
    starting with the varibles

--- projectWinterBeforeBreak.cpp
+++ projectWinterBeforeBreak.cpp
@@ -1,10 +1,13 @@
+//Player starting health would be 100, 3 enemy varibles randomised, always back and forth, 3 rounds total, only go to shop twice, six toal iteams, enemy health and attack gets higer each round, incorperate i into basicaly everything, 
 #include <iostream>
 #include <fstream> 
 #include <vector>
 #include <string>
 #include <iomanip>
+//Varibles
+int playerH, gold, enemyH1, enemyH2, enemyH3, playerA, playerD;
 
-//Player starting health would be 100, 3 enemy varibles randomised, always back and forth, 3 rounds total, only go to shop twice, six toal iteams, enemy health and attack gets higer each round, incorperate i into basicaly everything, everything is stat based, print results on to an outfile. 
+
 
 using namespace std;
 
