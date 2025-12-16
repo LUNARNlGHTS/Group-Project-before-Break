ca3a0b59f2eaf9ae99250b28f3aada5de0cf411c
austin-hk <austinhekk@gmail.com>
2025-12-16 10:37:56 -0500

    update towards main
    yea

--- projectWinterBeforeBreak.cpp
+++ projectWinterBeforeBreak.cpp
@@ -1,18 +1,20 @@
-//Player starting health would be 100, 3 enemy varibles randomised, always back and forth, 3 rounds total, only go to shop twice, six toal iteams, enemy health and attack gets higer each round, incorperate i into basicaly everything, 
 #include <iostream>
 #include <fstream> 
 #include <vector>
 #include <string>
 #include <iomanip>
-//Varibles
-
-
-
 
 using namespace std;
 
 int main() {
-int playerH, gold, enemyH1, enemyH2, enemyH3, playerA, playerD;
+
+    // initial variables - player
+    int playerHealth, playerGold, playerStatus, playerDefense, playerAttack;
+
+    // initial variables - enemies
+    int enemy1Health, enemy1Gold, enemy1Status, enemy1Defense, enemy1Attack;
+    int enemy2Health, enemy2Gold, enemy2Status, enemy2Defense, enemy2Attack;
+    int enemy3Health, enemy3Gold, enemy3Status, enemy3Defense, enemy3Attack;
  
     
 }
