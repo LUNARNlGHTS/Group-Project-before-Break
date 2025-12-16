6084a2548f7ec3336d57412e1f34884b796e9a2d
Athon Dulin <90697@wws.k12.in.us>
2025-12-16 10:30:27 -0500

    Update athonProjectMain.cpp

---
+++ athonProjectMain.cpp
@@ -0,0 +1,18 @@
+//Player starting health would be 100, 3 enemy varibles randomised, always back and forth, 3 rounds total, only go to shop twice, six toal iteams, enemy health and attack gets higer each round, incorperate i into basicaly everything, 
+#include <iostream>
+#include <fstream> 
+#include <vector>
+#include <string>
+#include <iomanip>
+//Varibles
+
+
+
+
+using namespace std;
+
+int main() {
+int playerH, gold, enemyH1, enemyH2, enemyH3, playerA, playerD, enemyA1, enemyA2, enemyA3, enemyD1, enemyD2, enemyD3;
+ 
+    
+}
