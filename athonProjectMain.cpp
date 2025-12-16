fe9421bc73dcf9587def56d5e346fb0d77b33644
Athon Dulin <90697@wws.k12.in.us>
2025-12-16 10:47:42 -0500

    Update athonProjectMain.cpp

--- athonProjectMain.cpp
+++ athonProjectMain.cpp
@@ -1,18 +1,30 @@
-//Player starting health would be 100, 3 enemy varibles randomised, always back and forth, 3 rounds total, only go to shop twice, six toal iteams, enemy health and attack gets higer each round, incorperate i into basicaly everything, 
 #include <iostream>
 #include <fstream> 
 #include <vector>
 #include <string>
 #include <iomanip>
-//Varibles
-
-
-
 
 using namespace std;
-
+//WE DON'T WANT THE PLAY TO GET ANTHING TOO GOOD AT THE START.
 int main() {
-int playerH, gold, enemyH1, enemyH2, enemyH3, playerA, playerD, enemyA1, enemyA2, enemyA3, enemyD1, enemyD2, enemyD3;
+
+    // initial variables - player
+    int playerHealth, playerGold, playerStatus, playerDefense, playerAttack;
+    string playerClass
+
+    // initial variables - enemies
+    int enemy1Health, enemy1Gold, enemy1Status, enemy1Defense, enemy1Attack;
+    int enemy2Health, enemy2Gold, enemy2Status, enemy2Defense, enemy2Attack;
+    int enemy3Health, enemy3Gold, enemy3Status, enemy3Defense, enemy3Attack;
+
+    // class declarations for basic, glass cannon, and tank
+
+
+    // main menu code would be here, including name specifics and class choosing
+
+    // assuming general combat code would be here, cycling 3 enemies before going to the shop
+
+    // shop code would be here, then combat would repeat same with shop every 3 rounds
  
     
 }
