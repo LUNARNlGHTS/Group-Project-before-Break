ba8d2830411a23ea501b60a45805cf7f6f315a90
austin-hk <austinhekk@gmail.com>
2025-12-16 10:01:49 -0500

    the best update
    tuff

--- projectWinterBeforeBreak.cpp
+++ projectWinterBeforeBreak.cpp
@@ -8,4 +8,15 @@ using namespace std;
 
 int main() {
 
+    int numberTuff;
+
+    cout << "What is 6 + 7?" << endl;
+    cin >> numberTuff;
+
+    if (numberTuff == 41 || numberTuff == 67) {
+        cout << "Tuff boi" << endl;
+    } else {
+        cout << "NAH!!!!!!" << endl;
+    }
+    
 }
