ac75696d5af77b4ed1db1fa9fad7ec514947aaaf
Athon Dulin <90697@wws.k12.in.us>
2025-12-17 10:43:47 -0500

    Update athonProjectMain.cpp
    Finished??

--- athonProjectMain.cpp
+++ athonProjectMain.cpp
@@ -10,14 +10,41 @@ int main() {
 
     // initial variables - player
     int playerHealth, playerGold, playerStatus, playerDefense, playerAttack;
-    string playerClass
+    string playerClass, playerName;
 
     // initial variables - enemies
     int enemy1Health, enemy1Gold, enemy1Status, enemy1Defense, enemy1Attack;
     int enemy2Health, enemy2Gold, enemy2Status, enemy2Defense, enemy2Attack;
     int enemy3Health, enemy3Gold, enemy3Status, enemy3Defense, enemy3Attack;
 
+    // player name input
+    cout << "Please enter your name: ";
+    cin >> playerName;
+
     // class declarations for basic, glass cannon, and tank
+    cout << "What class would you like?: (Adventurer, Wizard, Knight)";
+    cin >> playerClass;
+    if(playerClass == "Adventurer"){
+        playerHealth = 100;
+        playerAttack = 12;
+        playerDefense = 10;
+    }
+    if(playerClass == "Wizard"){
+        playerHealth = 90;
+        playerAttack = 15;
+        playerDefense = 5;
+    }
+    if(playerClass == "Knight"){
+        playerHealth = 120;
+        playerAttack = 15;
+        playerDefense = 20;
+    }
+    while(playerClass != "Adventurer" && playerClass != "Wizard" && playerClass != "Knight"){
+        cout << "Please choose a class (Case sensitive)";
+        cin >> playerClass;
+    }
+
+
 
 
     // main menu code would be here, including name specifics and class choosing
