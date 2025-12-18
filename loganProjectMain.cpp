01a057146de1949b3db441f63af5e377f1859a80
LUNARNlGHTS <loganmcnamee@icloud.com>
2025-12-18 11:10:17 -0500

    merged code

--- loganProjectMain.cpp
+++ loganProjectMain.cpp
@@ -1,6 +1,5 @@
 /*Still need a working looping menu that has the options ot start a new adventure then lead to 
-character select or exit the program. Add a counter of some sorts maybe a total rounds cleared or
-enemies beaten. Add data validation to every user input with while loops*/
+character select or exit the program.*/
 
 #include <iostream>
 #include <fstream> 
@@ -19,84 +18,193 @@ using namespace std;
 
 int main() {
 
-    // initial variables - player
-    int playerHealth, playerStatus, playerDefense, playerAttack;
-    int playerGold = 10;
-    string playerClass, playerName;
-
-
-
-
-    // player name input
-    cout << "Please enter your name: ";
-    cin >> playerName;
-
-    // class declarations for basic, glass cannon, and tank
-    cout << "What class would you like?: (Adventurer, Wizard, Knight)";
-    cin >> playerClass;
-    if(playerClass == "Adventurer"){
-        playerHealth = 100;
-        playerAttack = 12;
-        playerDefense = 10;
-    }
-    if(playerClass == "Wizard"){
-        playerHealth = 90;
-        playerAttack = 15;
-        playerDefense = 5;
-    }
-    if(playerClass == "Knight"){
-        playerHealth = 120;
-        playerAttack = 15;
-        playerDefense = 20;
-    }
-    while(playerClass != "Adventurer" && playerClass != "Wizard" && playerClass != "Knight"){
-        cout << "Please choose a class (Case sensitive)";
-        cin >> playerClass;
-    }
-    cout << "Welcome " << playerName << " " << "to Half-Life 3!";
-
-
-
-
-
-
-
-
+     // initial variables - player
+    int playerHealth, playerGold, playerStatus, playerDefense, playerAttack, playerClass, playerClassInfoSelection;
+    string playerName;
 
     // initial variables - enemies
     int enemyHealth, enemyGold, enemyStatus, enemyDefense, enemyAttack;
 
     // initial variables - checks
     bool menuStatus, shopStatus, fightStatus, playerHasInput;
+    menuStatus = true;
+    playerClassInfoSelection = 0;
 
     // variables for main fight section
-    int currentPlayerSelection, currentPlayerItemSelection, damageDealtPlayer, damageDealtEnemy;
+    int currentPlayerSelection, damageDealtPlayer, damageDealtEnemy, powerStrikeTurn, totalRounds;
     currentPlayerSelection = 0;
+    powerStrikeTurn = 0;
+    
+    while (menuStatus == true && playerClassInfoSelection == 0) {
+        cout << "\n\n\n\n\n\n" << endl;
+
+        // main prompting
+        cout << "What class would you like to play?\nEach has unique stats that are viewed upon selecting one." << endl;
+        cout << "[1] - Adventurer       [2] - Wizard" << endl;
+        cout << "[3] - Knight" << endl;
+        cout << "Selection: ";
+        cin >> playerClass;
+        
+        cout << "\n\n\n\n\n\n" << endl;
+
+        if (playerClass == 1) {
+
+            // specifying
+            playerHealth = 100;
+            playerAttack = 13;
+            playerDefense = 10;
+
+            // info
+            cout << "The Adventurer has mainly average stats, being useful in many categories." << endl;
+            cout << "Health:  " << playerHealth << endl;
+            cout << "Attack:  " << playerAttack << endl;
+            cout << "Defense: " << playerDefense << "\n" << endl;
+
+            cout << "[1] - Select             [0] - Return" << endl;
+            cout << "Selection: ";
+            cin >> playerClassInfoSelection;
+
+            if (playerClassInfoSelection == 1) {
+                
+                cout << "\n\n\n\n\n\n" << endl;
+                cout << "What is your name? (this will be used to show records of your progress!)" << endl;
+                cout << "Adventurer ";
+                cin >> playerName;
+
+                menuStatus = false;
+                shopStatus = false;
+                fightStatus = true;
+
+            } else {
+                playerClassInfoSelection = 0;
+            }
+
+        } else if (playerClass == 2) {
+
+            // specifying
+            playerHealth = 90;
+            playerAttack = 22;
+            playerDefense = 5;
+
+            // info
+            cout << "The Wizard has a high attack power, but lower defense power compared to other characters." << endl;
+            cout << "Health:  " << playerHealth << endl;
+            cout << "Attack:  " << playerAttack << endl;
+            cout << "Defense: " << playerDefense << "\n" << endl;
+
+            cout << "[1] - Select             [0] - Return" << endl;
+            cout << "Selection: ";
+            cin >> playerClassInfoSelection;
+
+            if (playerClassInfoSelection == 1) {
+
+                cout << "\n\n\n\n\n\n" << endl;
+                cout << "What is your name? (this will be used to show records of your progress!)" << endl;
+                cout << "Wizard ";
+                cin >> playerName;
+
+                menuStatus = false;
+                shopStatus = false;
+                fightStatus = true;
+
+            } else {
+                playerClassInfoSelection = 0;
+            }
+
+        } else if (playerClass == 3) {
+
+            // specifying
+            playerHealth = 120;
+            playerAttack = 8;
+            playerDefense = 18;
+
+            // info
+            cout << "The Knight is a brute, who has high defense and health, although having a lower attack." << endl;
+            cout << "Health:  " << playerHealth << endl;
+            cout << "Attack:  " << playerAttack << endl;
+            cout << "Defense: " << playerDefense << "\n" << endl;
+
+            cout << "[1] - Select             [0] - Return" << endl;
+            cout << "Selection: ";
+            cin >> playerClassInfoSelection;
+
+            if (playerClassInfoSelection == 1) {
+
+                cout << "\n\n\n\n\n\n" << endl;
+                cout << "What is your name? (this will be used to show records of your progress!)" << endl;
+                cout << "Knight ";
+                cin >> playerName;
+
+                menuStatus = false;
+                shopStatus = false;
+                fightStatus = true;
+
+            } else {
+                playerClassInfoSelection = 0;
+            }
+
+        }
+        
+}
+
+    // declaring things for fighting (MUST be active for the thing function)
     playerHasInput = false;
     shopStatus = false;
     menuStatus = false;
+    totalRounds = 1;
+
+    // inital enemy creation, gets effected per each round progressed
+    int baseEnemyAttack = 10;
+    int baseEnemyHealth = 35;
+    int baseEnemyDefense = 3;
+    enemyAttack = baseEnemyAttack;
+    enemyHealth = baseEnemyHealth;
+    enemyDefense = baseEnemyDefense;
+
 
     while (currentPlayerSelection == 0 && shopStatus == false && menuStatus == false) {
+
         fightStatus = true;
         cout << "\n\n\n\n\n\n" << endl; // both of these are for new line padding, looks better
+
+        if (enemyHealth <= 0) {
+
+            totalRounds = totalRounds + 1;
+
+            cout << "You defeated the enemy! Another one approaches..." << endl;
+            powerStrikeTurn = powerStrikeTurn + 1;
+            sleep_for(seconds(3));
+            cout << "\n\n\n\n\n\n";
+
+            enemyAttack = baseEnemyAttack * pow(1.1, totalRounds);
+            enemyHealth = baseEnemyHealth * pow(1.1, totalRounds);
+            enemyDefense = baseEnemyDefense * pow(1.1, totalRounds);
+
+            playerHasInput = false;
+            currentPlayerSelection = 0;
+
+        }
 
         if (playerHasInput == true) {
 
             while (playerHasInput == true) {
 
-                enemyAttack = damageDealtEnemy;
+                damageDealtEnemy = enemyAttack;
+                playerHealth = playerHealth - damageDealtEnemy;
                 cout << "You took " << damageDealtEnemy << " points of damage!" << endl;
                 sleep_for(seconds(1));
                 currentPlayerSelection = 0;
                 playerHasInput = false;
+                powerStrikeTurn = powerStrikeTurn + 1;
 
             }
 
         } else if (playerHasInput == false) {
 
             // main battle scene & input
+            cout << "HP: " << playerHealth << "  ATK: " << playerAttack << "  DEF: " << playerDefense << "  ROUND: " << totalRounds << endl;
             cout << "An enemy appears! What will you do?" << endl;
-            cout << "[1] - Fight            [2] - Item" << endl;
+            cout << "[1] - Fight            [2] - Power Strike" << endl;
             cout << "[3] - Inspect          [4] - Defend" << endl;
             cout << "Selection: ";
             cin >> currentPlayerSelection;
@@ -108,7 +216,8 @@ int main() {
         while (currentPlayerSelection == 1 && playerHasInput == false) {
 
             // player attacking
-            playerAttack = damageDealtPlayer;
+            damageDealtPlayer = playerAttack;
+            enemyHealth = enemyHealth - damageDealtPlayer;
             cout << damageDealtPlayer << " damage was done to the enemy!" << endl;
             sleep_for(seconds(1));
             playerHasInput = true;
@@ -117,25 +226,67 @@ int main() {
         }
         while (currentPlayerSelection == 2) {
 
+            if (powerStrikeTurn >= 3) {
+
+                // power strike attacking
+                damageDealtPlayer = playerAttack;
+                damageDealtPlayer = damageDealtPlayer * 3;
+                enemyHealth = enemyHealth - damageDealtPlayer;
+                cout << "A power strike of " << damageDealtPlayer << " damage was done to the enemy!!" << endl;
+                sleep_for(seconds(1));
+
+                // resetting
+                powerStrikeTurn = powerStrikeTurn - 4;
+                damageDealtPlayer = playerAttack;
+                playerHasInput = true;
+                currentPlayerSelection = 0;
+
+            } else if (powerStrikeTurn <= 2) {
+
+                // if player hasn't charged power strike for 3 rounds
+                cout << "You need to charge your power strike for 3 rounds (currently " << powerStrikeTurn << ") in order to power strike!" << endl;
+                cout << "\n[0] - Quit" << endl;
+                cout << "Selection: ";
+                cin >> currentPlayerSelection;
+
+            }
+            
         }
+
         while (currentPlayerSelection == 3) {
 
-            cout << "Current enemies stats" << endl;
+            // stat displaying
+            cout << "Stats of the current enemy" << endl;
+            cout << "HP:  " << enemyHealth << endl;
+            cout << "ATK:  " << enemyAttack << endl;
+            cout << "DEF: " << enemyDefense << endl;
+            cout << "STATUS:  " << enemyStatus << endl;
+            cout << "\n[0] - Quit" << endl;
+            cout << "Selection: ";
+            cin >> currentPlayerSelection;
+
+        }
+
+        while (currentPlayerSelection == 4) {
+
+            cout << "Stats of the current enemy" << endl;
             cout << "Health:  " << enemyHealth << endl;
             cout << "Attack:  " << enemyAttack << endl;
             cout << "Defense: " << enemyDefense << endl;
             cout << "Status:  " << enemyStatus << endl;
-            sleep_for(seconds(8));
-            playerHasInput = true;
+            cout << "\n[0] - Quit" << endl;
+            cout << "Selection: ";
+            cin >> currentPlayerSelection;
+
+        }
+
+        while (currentPlayerSelection >= 5) {
+
             currentPlayerSelection = 0;
 
         }
+
     }
-
-
-
-
-
 
 
 
