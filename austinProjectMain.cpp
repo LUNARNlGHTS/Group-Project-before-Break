4a04cf7aaab4bf878bd77e578508498630591a78
austin-hk <austinhekk@gmail.com>
2025-12-19 10:49:23 -0500

    aetwgsdgteagwe
    wewehrwehrhne

--- austinProjectMain.cpp
+++ austinProjectMain.cpp
@@ -9,13 +9,16 @@
 #include <thread>
 #include <random>
 
-// for waiting, and standard specifications
 using namespace std::this_thread;
 using namespace std::chrono;
 using namespace std;
 
 int main() {
-    
+
+    ////////////////////////////
+    //   VAR / INIT SECTION   //
+    ////////////////////////////
+
     // for game restarting
     bool gameRunning = true;
 
@@ -29,22 +32,54 @@ int main() {
     int enemyHealth, enemyGold, enemyStatus, enemyDefense, enemyAttack;
 
     // initial variables - checks
-    bool menuStatus, shopStatus, fightStatus, playerHasInput;
+    bool menuStatus, characterSelectStatus, shopStatus, fightStatus, playerHasInput;
     menuStatus = true;
     playerClassInfoSelection = 0;
 
     // variables for main fight section
-    int currentPlayerSelection, damageDealtPlayer, damageDealtEnemy, powerStrikeTurn, totalRounds, playerGameOverSelection;
+    int menuSelection, currentPlayerSelection, damageDealtPlayer, damageDealtEnemy, powerStrikeTurn, totalRounds, playerGameOverSelection;
     currentPlayerSelection = 0;
     powerStrikeTurn = 0;
+
+    ///////////////////////////
+    //   MAIN MENU SECTION   //
+    ///////////////////////////
+    
+   while (menuStatus == true) {
+
+      while (menuSelection > 2 || menuSelection < 1) {
+
+         cout << "\n\n\n\n\n\n" << endl;
+
+         // main menu printing
+         cout << "Half Life 3" << endl;
+         cout << "\n" << endl;
+         cout << "[1] - Start Adventure" << endl;
+         cout << "[2] - Exit" << endl;
+         cout << "Selection: ";
+         cin >> menuSelection;
+
+      }
+
+      if (menuSelection == 1) {
+
+         menuStatus = false;
+         characterSelectStatus = true;
+
+      } else {
+
+         gameRunning = false;
+         return 0;
+
+      }
+
+   }
 
     ////////////////////////////
     //   MAIN CLASS SECTION   //
     ////////////////////////////
     
-
-    
-    while (menuStatus == true && playerClassInfoSelection == 0) {
+    while (characterSelectStatus == true && playerClassInfoSelection == 0) {
         cout << "\n\n\n\n\n\n" << endl;
 
         // main prompting
@@ -80,7 +115,7 @@ int main() {
                 cout << "Adventurer ";
                 cin >> playerName;
 
-                menuStatus = false;
+                characterSelectStatus = false;
                 shopStatus = false;
                 fightStatus = true;
 
@@ -114,7 +149,7 @@ int main() {
                 cout << "Wizard ";
                 cin >> playerName;
 
-                menuStatus = false;
+                characterSelectStatus = false;
                 shopStatus = false;
                 fightStatus = true;
 
@@ -146,7 +181,7 @@ int main() {
                 cout << "Knight ";
                 cin >> playerName;
 
-                menuStatus = false;
+                characterSelectStatus = false;
                 shopStatus = false;
                 fightStatus = true;
 
@@ -166,11 +201,12 @@ int main() {
     playerHasInput = false;
     shopStatus = false;
     menuStatus = false;
+    characterSelectStatus = false;
     fightStatus = true;
     totalRounds = 1;
 
     // inital enemy creation, gets effected per each round progressed
-    int baseEnemyAttack = 10;
+    int baseEnemyAttack = 10000;
     int baseEnemyHealth = 35;
     int baseEnemyDefense = 3;
     enemyAttack = baseEnemyAttack;
@@ -182,7 +218,7 @@ int main() {
     int basePlayerHealth = playerHealth;
     int basePlayerDefense = playerDefense;
 
-    while (currentPlayerSelection == 0 && shopStatus == false && menuStatus == false && fightStatus == true) {
+    while (currentPlayerSelection == 0 && shopStatus == false && menuStatus == false && fightStatus == true && characterSelectStatus == false) {
 
         fightStatus = true;
         cout << "\n\n\n\n\n\n" << endl; // both of these are for new line padding, looks better
@@ -202,6 +238,7 @@ int main() {
                 playerHasInput = false;
                 shopStatus = false;
                 menuStatus = false;
+                characterSelectStatus = false;
                 playerClassInfoSelection = 0;
                 
                 // enemy resetting
@@ -218,8 +255,10 @@ int main() {
                 
                 // status resetting
                 menuStatus = true;
+                characterSelectStatus = false;
                 fightStatus = false;
                 shopStatus = false;
+                menuSelection = 0;
 
                 // player input resetting
                 playerClassInfoSelection = 0;
@@ -383,9 +422,183 @@ int main() {
             currentPlayerSelection = 0;
 
         }
-
+      
     }
     
+    ///////////////////////////
+    //   MAIN SHOP SECTION   //
+    ///////////////////////////
+    
+    // shop selection options
+    string shopOpt1, shopOpt2, shopOpt3;
+
+    // shop items
+    string shopItem1 = "Health Potion: Heals 20 HP";
+    string shopItem2 = "Mega Health Potion: Heals 50 HP";
+    string shopItem3 = "Dull Sword: Increases ATK by 3";
+    string shopItem4 = "Crowbar: Increases ATK by 10";
+    string shopItem5 = "Wooden Shield: Increases DEF by 3";
+    string shopItem6 = "Iron Shield: Increases DEF by 10";
+
+    // shop pricing
+    int shopPrice1;
+    int shopPrice2;
+    int shopPrice3;
+
+    // shop choosing
+    int shopChoice;
+
+    // random elements for shopping
+    random_device rd;
+    mt19937 rng(rd()); 
+    uniform_int_distribution<int> uni(1, 100);
+    int randomInteger = uni(rng);
+
+    while (shopStatus == true) {
+      
+    // decide the three items going to be in the shop from a list of 8 items 2 to increase each stat and 2 health potions with two tiers for each stat and potion
+    for (int i = 0; i<3; i++) {
+
+      if (i==0){
+         if(randomInteger >= 90){
+            shopOpt1 = shopItem2;
+            shopPrice1 = 67;
+         } else if (randomInteger >= 80){
+            shopOpt1 = shopItem4;
+            shopPrice1 = 67;
+         } else if (randomInteger >= 70){
+            shopOpt1 = shopItem6;
+            shopPrice1 = 67;
+         } else if (randomInteger >= 50){
+            shopOpt1 = shopItem3;
+            shopPrice1 = 20;
+         } else if (randomInteger >= 30){
+            shopOpt1 = shopItem5;
+            shopPrice1 = 20;
+         } else {
+            shopOpt1 = shopItem1;
+            shopPrice1 = 15;
+         }
+      } else if (i==1){
+         if(randomInteger >= 90){
+            shopOpt2 = shopItem2;
+            shopPrice2 = 67;
+         } else if (randomInteger >= 80){
+            shopOpt2 = shopItem4;
+            shopPrice2 = 67;
+         } else if (randomInteger >= 70){
+            shopOpt2 = shopItem6;
+            shopPrice2 = 67;
+         } else if (randomInteger >= 50){
+            shopOpt2 = shopItem3;
+            shopPrice2 = 20;
+         } else if (randomInteger >= 30){
+            shopOpt2 = shopItem5;
+            shopPrice2 = 20;
+         } else {
+            shopOpt2 = shopItem1;
+            shopPrice2 = 15;
+         }
+      } else {
+         if(randomInteger >= 90){
+            shopOpt3 = shopItem2;
+            shopPrice3 = 67;
+         } else if (randomInteger >= 80){
+            shopOpt3 = shopItem4;
+            shopPrice3 = 67;
+         } else if (randomInteger >= 70){
+            shopOpt3 = shopItem6;
+            shopPrice3 = 67;
+         } else if (randomInteger >= 50){
+            shopOpt3 = shopItem3;
+            shopPrice3 = 20;
+         } else if (randomInteger >= 30){
+            shopOpt3 = shopItem5;
+            shopPrice3 = 20;
+         } else {
+            shopOpt3 = shopItem1;
+            shopPrice3 = 15;
+         }
+      }
+      randomInteger = uni(rng);
+   }
+
+    cout << "Welcome to the shop what would you like to buy" << endl;
+    cout << "1. " << shopOpt1 << " $" << shopPrice1 << " 2. " << shopOpt2 << " $" << shopPrice2 << endl;
+    cout << "3. " << shopOpt3 << " $" << shopPrice3 << " 4. Leave" << endl;
+    cin >> shopChoice;
+    while (shopChoice < 1 || shopChoice > 4){
+      cout << "Please enter a valid choice" << endl;
+      cout << "1. " << shopOpt1 << " $" << shopPrice1 << " 2. " << shopOpt2 << " $" << shopPrice2  << endl;
+      cout << "3. " << shopOpt3 << " $" << shopPrice3 << " 4. Leave" << endl;
+      cin >> shopChoice; 
     }
+    if (shopChoice == 1 && playerGold >= shopPrice1){
+      if (shopOpt1 == shopItem1){
+         playerHealth = playerHealth + 20;
+         playerGold = playerGold - shopPrice1;
+      } else if (shopOpt1 == shopItem2){
+         playerHealth = playerHealth + 50;
+         playerGold = playerGold - shopPrice1;
+      } else if (shopOpt1 == shopItem3){
+         playerAttack = playerAttack + 3;
+         playerGold = playerGold - shopPrice1;
+      } else if (shopOpt1 == shopItem4){
+         playerAttack = playerAttack + 10;
+         playerGold = playerGold - shopPrice1;
+      } else if(shopOpt1 == shopItem5){
+         playerDefense = playerDefense + 3;
+         playerGold = playerGold - shopPrice1;
+      } else {
+         playerDefense = playerDefense + 10;
+         playerGold = playerGold - shopPrice1;
+      }
+    } else if (shopChoice == 2 && playerGold >= shopPrice2){
+      if (shopOpt2 == shopItem1){
+         playerHealth = playerHealth + 20;
+         playerGold = playerGold - shopPrice2;
+      } else if (shopOpt2 == shopItem2){
+         playerHealth = playerHealth + 50;
+         playerGold = playerGold - shopPrice2;
+      } else if (shopOpt2 == shopItem3){
+         playerAttack = playerAttack + 3;
+         playerGold = playerGold - shopPrice2;
+      } else if (shopOpt2 == shopItem4){
+         playerAttack = playerAttack + 10;
+         playerGold = playerGold - shopPrice2;
+      } else if(shopOpt2 == shopItem5){
+         playerDefense = playerDefense + 3;
+         playerGold = playerGold - shopPrice2;
+      } else {
+         playerDefense = playerDefense + 10;
+         playerGold = playerGold - shopPrice2;
+      }
+    } else if (shopChoice == 3 && playerGold >= shopPrice3){
+      if (shopOpt3 == shopItem1){
+         playerHealth = playerHealth + 20;
+         playerGold = playerGold - shopPrice3;
+      } else if (shopOpt3 == shopItem2){
+         playerHealth = playerHealth + 50;
+         playerGold = playerGold - shopPrice3;
+      } else if (shopOpt3 == shopItem3){
+         playerAttack = playerAttack + 3;
+         playerGold = playerGold - shopPrice3;
+      } else if (shopOpt3 == shopItem4){
+         playerAttack = playerAttack + 10;
+         playerGold = playerGold - shopPrice3;
+      } else if(shopOpt3 == shopItem5){
+         playerDefense = playerDefense + 3;
+         playerGold = playerGold - shopPrice3;
+      } else {
+         playerDefense = playerDefense + 10;
+         playerGold = playerGold - shopPrice3;
+      }
+    } else {
+      return 0;
+    }
+    shopStatus = false;
+   }
+
+}
 
 }
